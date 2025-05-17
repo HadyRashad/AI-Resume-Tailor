07df59a8c0eb8e0a48df840cd4087f9a11f84416
Hady Rashad <hadyr21@outlook.com>
2025-05-17 20:22:36 +0300

    Fresh Start

---
+++ backend/app-locked.py
@@ -0,0 +1,2 @@
+# This file is locked for confidentiality.
+# If you need to access the code, please contact the administrator.
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        
+        <div className="footer-bottom">
+          <p></p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
